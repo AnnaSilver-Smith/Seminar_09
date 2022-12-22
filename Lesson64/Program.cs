fb8ab5882239c6107631764fa31bca9cb3552f06
Anna <anna.designer8@yandex.ru>
2022-12-22 21:46:32 +0500

    first commit

--- Lesson64/Program.cs
+++ Lesson64/Program.cs
@@ -1,16 +1,17 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
-Console.Write("Введите натуральное число 1: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.Write("Ведите натуральное число 2: ");
+Console.Write("Введите натуральное число M: ");
 int M = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"N={N}, M={M}");
-for (int i = N; i <= M; i++)
-    if (i < M)
+Console.Write("Ведите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Натуральные числа от M = {M}, N = {N} -> ");
+for (int i = M; i <= N; i++)
+{
+    if (i < N)
     {
         Console.Write($" {i}");
     }
-for (int i = M; i <= N; i++)
     if (i == N)
     {
         Console.Write($" {i}");
     }
+}
