f7ab5630e0a514bc00dce6e9f966f306a7c47858
Anna <anna.designer8@yandex.ru>
2022-12-22 21:03:35 +0500

    first commit

---
+++ Lesson66/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+using System.Numerics;
+
+Console.Write("Введите натуральное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (M > N || M < 1)
+{
+    if (M > N)
+    {    
+        M = Convert.ToInt32(Console.ReadLine());      
+        N = Convert.ToInt32(Console.ReadLine());
+    }
+    if (M < 1)
+    {     
+        M = Convert.ToInt32(Console.ReadLine());     
+        N = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+
+
+//Через цикл
+
+for (int i = M; i <= N; i++)
+{
+    if (i <= N)
+    {
+        sum += i;
+    }
+}
+
+Console.WriteLine($"Result 1: Сумма натуральных элементов от {M} до {N} -> {sum}");
+
+
+//Через рекурсию
+
+SumElement(M, N, sum);
+void SumElement(int M, int N, int sum)
+{
+    if (M == N)
+    {
+        sum += M;
+       
+    }
+    if (M < N) 
+    {
+        sum += M;
+        SumElement(M + 1, N, sum);
+    }
+}
+Console.WriteLine($"Result 2: Сумма натуральных элементов от {M} до {N} -> {sum}");
---
+++ Lesson68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.Write("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+static int A(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+        if (m != 0 && n == 0)
+    {
+        return A(m - 1, 1);
+    }
+    else
+        return A(m - 1, A(m, n - 1));
+}
+Console.WriteLine($"Функция Акермана с неотрицательными числами А({m}, {n}) равна {A(m, n)}");
